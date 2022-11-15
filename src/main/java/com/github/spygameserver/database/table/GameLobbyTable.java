3aecb2ca20510b95b25dfd246f76c51d3bd1e03f
Kalkra11235 <bear.addr@att.net>
2022-11-15 11:27:16 -0800

    updated game lobby table class

--- src/main/java/com/github/spygameserver/database/table/GameLobbyTable.java
+++ src/main/java/com/github/spygameserver/database/table/GameLobbyTable.java
@@ -16,12 +16,12 @@ public class GameLobbyTable extends AbstractTable {
 
     private static final String CREATE_TABLE_QUERY = "CREATE TABLE IF NOT EXISTS %s (game_id INT NOT NULL" +
             "AUTO_INCREMENT, invite_code CHAR(6), is_public INT, game_type ENUM, max_players INT," +
-            "current_players INT, start_time INT, end_time INT, PRIMARY KEY (game_id), UNIQUE (invite_code))";
+            "current_players INT, start_time BIGINT, end_time BIGINT, PRIMARY KEY (game_id), UNIQUE (invite_code))";
 
-    private static final String DOES_INVITE_CODE_EXIST_QUERY = "SELECT 1 FROM %s WHERE invite_code=?";
+    private static final String GAME_FROM_INVITE_CODE_QUERY = "SELECT game_id, start_time FROM %s WHERE invite_code=?";
 
     private static final String INSERT_INTO_QUERY = "INSERT INTO %s (invite_code, is_public, game_type, " +
-            "max_players, current_players, start_time, end_time) VALUES (?, ?, ?, ?, 0, -1, -1)";
+            "max_players, current_players) VALUES (?, ?, ?, ?, ?)";
 
     private static final String UPDATE_CURRENT_PLAYERS_QUERY = "UPDATE %s SET current_players=? WHERE game_id=?";
     private static final String UPDATE_START_TIME = "UPDATE %s SET start_time=? WHERE game_id=?";
@@ -47,26 +47,29 @@ public class GameLobbyTable extends AbstractTable {
         connectionHandler.closeConnectionIfNecessary();
     }
 
-    public boolean checkInviteCode(ConnectionHandler connectionHandler, String invite_code) {
-        return doesUniquePropertyAlreadyExist(connectionHandler, DOES_INVITE_CODE_EXIST_QUERY, invite_code);
-    }
-
-    private boolean doesUniquePropertyAlreadyExist(ConnectionHandler connectionHandler, String query, String property) {
+    public Pair<Integer, Long> getGameIdFromInviteCode(ConnectionHandler connectionHandler, String invite_code) {
         Connection connection = connectionHandler.getConnection();
-        String selectOneQuery = formatQuery(query);
+        String selectOneQuery = formatQuery(GAME_FROM_INVITE_CODE_QUERY);
+
+        Integer gameId = null;
+        Long startTime = null;
 
         try (PreparedStatement preparedStatement = connection.prepareStatement(selectOneQuery)) {
-            preparedStatement.setString(1, property);
+            preparedStatement.setString(1, invite_code);
 
             try (ResultSet resultSet = preparedStatement.executeQuery()) {
                 // If there is a result, then this property does exist
-                return resultSet.next();
+                if(resultSet.next()) {
+                    gameId = resultSet.getInt(1);
+                    startTime = resultSet.getLong(2);
+                }
             }
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
 
-        return false;
+        connectionHandler.closeConnectionIfNecessary();
+        return new Pair<>(gameId, startTime);
     }
 
     public void createGame(ConnectionHandler connectionHandler, String invite_code, int is_public,
@@ -79,19 +82,7 @@ public class GameLobbyTable extends AbstractTable {
             preparedStatement.setInt(2, is_public);
             preparedStatement.setInt(3, game_type);
             preparedStatement.setInt(4, max_players);
-
-            preparedStatement.executeUpdate();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    public void checkInviteCode(ConnectionHandler connectionHandler, String invite_code) {
-        Connection connection = connectionHandler.getConnection();
-        String updateUsernameQuery = formatQuery(DOES_INVITE_CODE_EXIST_QUERY);
-
-        try (PreparedStatement preparedStatement = connection.prepareStatement(updateUsernameQuery)) {
-            preparedStatement.setString(1, invite_code);
+            preparedStatement.setInt(5, 0);
 
             preparedStatement.executeUpdate();
         } catch (SQLException ex) {
@@ -112,12 +103,12 @@ public class GameLobbyTable extends AbstractTable {
         }
     }
 
-    public void updateStartTime(ConnectionHandler connectionHandler, int start_time) {
+    public void updateStartTime(ConnectionHandler connectionHandler, long start_time) {
         Connection connection = connectionHandler.getConnection();
         String updateUsernameQuery = formatQuery(UPDATE_START_TIME);
 
         try (PreparedStatement preparedStatement = connection.prepareStatement(updateUsernameQuery)) {
-            preparedStatement.setInt(1, start_time);
+            preparedStatement.setLong(1, start_time);
 
             preparedStatement.executeUpdate();
         } catch (SQLException ex) {
@@ -125,12 +116,12 @@ public class GameLobbyTable extends AbstractTable {
         }
     }
 
-    public void updateEndTime(ConnectionHandler connectionHandler, int end_time) {
+    public void updateEndTime(ConnectionHandler connectionHandler, long end_time) {
         Connection connection = connectionHandler.getConnection();
         String updateUsernameQuery = formatQuery(UPDATE_END_TIME);
 
         try (PreparedStatement preparedStatement = connection.prepareStatement(updateUsernameQuery)) {
-            preparedStatement.setInt(1, end_time);
+            preparedStatement.setLong(1, end_time);
 
             preparedStatement.executeUpdate();
         } catch (SQLException ex) {
@@ -138,9 +129,23 @@ public class GameLobbyTable extends AbstractTable {
         }
     }
 
-    private interface SQLStatementConsumer {
+    public static class Pair<L, R> {
 
-        void consume(PreparedStatement preparedStatement) throws SQLException;
+        private final L l;
+        private final R r;
+
+        public Pair(L l, R r) {
+            this.l = l;
+            this.r = r;
+        }
+
+        public L getL() {
+            return l;
+        }
+
+        public R getR() {
+            return r;
+        }
 
     }
 
