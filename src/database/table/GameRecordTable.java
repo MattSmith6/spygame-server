c16ce09fdf8a057cd7b5281a6033e31660abae11
mariochoto <32563610+mariochoto@users.noreply.github.com>
2022-12-01 01:47:13 -0800

    https://mattsmith6.atlassian.net/browse/SPYG-84?focusedCommentId=10008
    updates to the GameRecordTable, still needs to change primary key to foreign. and develop the Unit test. 
     packet for the player created to get the game log for the game that they are currently in.

--- src/database/table/GameRecordTable.java
+++ src/database/table/GameRecordTable.java
@@ -13,23 +13,24 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Map;
 
 public class GameRecordTable extends AbstractTable {
     private static final String NON_TESTING_TABLE_NAME = "game_records";
 
-    private static final String CREATE_TABLE_QUERY = "CREATE TABLE IF NOT EXISTS %s (game_id INT NOT NULL, eliminator_id INT NOT NULL, eliminatee_id INT NOT NULL, " +
-            "elimination_time VARCHAR(15) NOT NULL, PRIMARY KEY (game_id))";
-    private static final String INSERT_INTO_QUERY = "INSERT INTO %s (    game_id,    eliminator_id,  eliminatee_id,    elimination_time) VALUES (?, ?, ?, ?)";
-    private static final String UPDATE_QUERY = "UPDATE %s SET eliminator_id=?,  eliminatee_id=?,    elimination_time=? WHERE game_id=?";
-    private static final String SELECT_QUERY = "SELECT     game_id,    eliminator_id,  eliminatee_id,    elimination_time FROM %s WHERE game_id=?";
+    private static final String CREATE_TABLE_QUERY = "CREATE TABLE IF NOT EXISTS %s (record_id, game_id INT NOT NULL, eliminator_id INT NOT NULL, eliminatee_id INT NOT NULL, " +
+            "elimination_time bigint NOT NULL, PRIMARY KEY (record_id))";
+    private static final String INSERT_INTO_QUERY = "INSERT INTO %s (    record_id, game_id,    eliminator_id,  eliminatee_id,    elimination_time) VALUES (?, ?, ?, ?, ?)";
+    private static final String UPDATE_QUERY = "UPDATE %s SET game_id=?, eliminator_id=?,  eliminatee_id=?,    elimination_time=? WHERE record_id=?";
+    private static final String SELECT_QUERY = "SELECT    record_id, game_id,    eliminator_id,  eliminatee_id,    elimination_time FROM %s WHERE game_id=?";
 
     public GameRecordTable(boolean useTestTables) {
         super(NON_TESTING_TABLE_NAME, useTestTables);
     }
 
-    //first function to create Table:
-    @Override
-    protected void createTableIfNotExists(ConnectionHandler connectionHandler) {
+//first funtion to create Table:
+@Override
+protected void createTableIfNotExists(ConnectionHandler connectionHandler) {
         Connection connection = connectionHandler.getConnection();
         String createTableIfNotExistsQuery = formatQuery(CREATE_TABLE_QUERY);
         try (PreparedStatement preparedStatement = connection.prepareStatement(createTableIfNotExistsQuery)) {
@@ -41,73 +42,79 @@ public class GameRecordTable extends AbstractTable {
         connectionHandler.closeConnectionIfNecessary();
     }
 
-    //Second Function Table to insert into table
-    public void insertGameRecord(ConnectionHandler connectionHandler, game_record record) {
-        Connection connection = connectionHandler.getConnection();
-        String insertIntoQuery = formatQuery(INSERT_INTO_QUERY);
+//Second Function Table to insert into table
+public void insertGameRecord(ConnectionHandler connectionHandler, game_record record) {
+    Connection connection = connectionHandler.getConnection();
+    String insertIntoQuery = formatQuery(INSERT_INTO_QUERY);
 
-        try (PreparedStatement preparedStatement = connection.prepareStatement(insertIntoQuery)) {
-            preparedStatement.setString(1, record.getGame_id());
-            preparedStatement.setString(2, record.getEliminator_id());
-            preparedStatement.setString(3, record.getEliminatee_id());
-            preparedStatement.setString(4, record.getElimination_time());
-            preparedStatement.executeUpdate();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-        connectionHandler.closeConnectionIfNecessary();
+    try (PreparedStatement preparedStatement = connection.prepareStatement(insertIntoQuery)) {
+        preparedStatement.setString(1, String.valueOf(record.getRecord_id()));
+        preparedStatement.setString(2, String.valueOf(record.getGame_id()));
+        preparedStatement.setString(3, String.valueOf(record.getEliminator_id()));
+        preparedStatement.setString(4, String.valueOf(record.getEliminatee_id()));
+        preparedStatement.setString(5, String.valueOf(record.getElimination_time()));
+        preparedStatement.executeUpdate();
+    } catch (SQLException ex) {
+        ex.printStackTrace();
     }
+    connectionHandler.closeConnectionIfNecessary();
+}
 
-    //Second Function Table to select from  table
-    private game_record selectGameRecord(ConnectionHandler connectionHandler, int game_id) {
-        Connection connection = connectionHandler.getConnection();
-        String selectOneQuery = formatQuery(SELECT_QUERY);
-        game_record dataObject = null;
-        try (PreparedStatement preparedStatement = connection.prepareStatement(selectOneQuery)) {
-            preparedStatement.setString(1, game_id);
+//Second Function Table to select from  table
+private Map<Integer, game_record> selectGameRecord(ConnectionHandler connectionHandler, int game_id) {
+    Connection connection = connectionHandler.getConnection();
+    String selectOneQuery = formatQuery(SELECT_QUERY);
+    game_record dataObject = null;
+    Map<Integer, game_record> objectList = null;
+    try (PreparedStatement preparedStatement = connection.prepareStatement(selectOneQuery)) {
+        preparedStatement.setString(1, String.valueOf(game_id));
 
-            try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                // If there is a result, then this property does exist
-                if (!resultSet.next()) {
-                    return null;
-                }
-                int gameID = resultSet.getInt(1);
-                int eliminatorID = resultSet.getInt(2);
-                int eliminateeID = resultSet.getInt(3);
-                String eliminationDate = resultSet.getString(4);
+        try (ResultSet resultSet = preparedStatement.executeQuery()) {
+            // If there is a result, then this property does exist
+            while(resultSet.next()) {
 
-                dataObject = new game_record(gameID,eliminatorID,eliminateeID,eliminationDate);
+            int record_id = resultSet.getInt(1);
+            int gameID = resultSet.getInt(2);
+            int eliminatorID = resultSet.getInt(3);
+            int eliminateeID = resultSet.getInt(4);
+            long eliminationDate =Long.parseLong(resultSet.getString(5));
+            dataObject = new game_record(record_id, gameID,eliminatorID,eliminateeID,eliminationDate);
+            objectList.put(game_id, dataObject);
             }
 
-        } catch (SQLException ex) {
-            ex.printStackTrace();
         }
 
-        connectionHandler.closeConnectionIfNecessary();
-        return dataObject;
+    } catch (SQLException ex) {
+        ex.printStackTrace();
     }
 
-    //Third Function Table to update the table
-    public void updateGameRecord(ConnectionHandler connectionHandler,int eliminator_id,int  eliminatee_id, String elimination_time, int game_id) {
-        Connection connection = connectionHandler.getConnection();
-        String updateUsernameQuery = formatQuery(UPDATE_QUERY);
-
-        try (PreparedStatement preparedStatement = connection.prepareStatement(updateUsernameQuery)) {
-            preparedStatement.setString(1, eliminator_id);
-            preparedStatement.setString(2,  eliminatee_id);
-            preparedStatement.setString(3, elimination_time);
-            preparedStatement.setString(4, game_id);
-
-            preparedStatement.executeUpdate();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-
-        connectionHandler.closeConnectionIfNecessary();
-    }
-
-
+    connectionHandler.closeConnectionIfNecessary();
+    return objectList;
 }
+
+//Third Function Table to update the table
+public void updateGameRecord(ConnectionHandler connectionHandler,int eliminator_id,int  eliminatee_id, long elimination_time, int game_id, int record_id) {
+    Connection connection = connectionHandler.getConnection();
+    String updateUsernameQuery = formatQuery(UPDATE_QUERY);
+
+    try (PreparedStatement preparedStatement = connection.prepareStatement(updateUsernameQuery)) {
+        preparedStatement.setString(1, String.valueOf(eliminator_id));
+        preparedStatement.setString(2, String.valueOf(eliminatee_id));
+        preparedStatement.setString(3, String.valueOf(elimination_time));
+        preparedStatement.setString(4, String.valueOf(game_id));
+        preparedStatement.setString(4, String.valueOf(record_id));
+
+        preparedStatement.executeUpdate();
+    } catch (SQLException ex) {
+        ex.printStackTrace();
+    }
+
+    connectionHandler.closeConnectionIfNecessary();
+}
+
+
+    }
+
 
 
 
--- src/database/table/game_record.java
+++ src/database/table/game_record.java
@@ -1,18 +1,23 @@
 package com.github.spygameserver.database.table;
 
+
+
 public class game_record {
+
     //class parameters
+    private int record_id;
     private int  game_id;
     private int  eliminator_id;
     private int  eliminatee_id;
-    private String elimination_time;
+    private long elimination_time;
 
     //Constructor
-    public game_record(int game_id,     int   eliminator_id,    int     eliminatee_id,    String    elimination_time) {
+    public game_record(int record_id, int game_id,     int   eliminator_id,    int     eliminatee_id,    long    elimination_time) {
         this.game_id           = game_id;
         this.eliminator_id     = eliminator_id;
         this.eliminatee_id     = eliminatee_id;
         this.elimination_time  = elimination_time;
+        this.record_id = record_id;
 
     }
     //Getter and Setter of All Parameters
@@ -40,12 +45,18 @@ public class game_record {
         this.eliminatee_id = eliminatee_id;
     }
 
-    public String getElimination_time() {
+    public long getElimination_time() {
         return elimination_time;
     }
 
-    public void setElimination_time(String elimination_time) {
+    public void setElimination_time(long elimination_time) {
         this.elimination_time = elimination_time;
     }
 
+    public int getRecord_id() {
+        return  record_id;
+    }
+    public void setRecord_id(int record_id){
+        this.record_id = record_id;
+    }
 }
